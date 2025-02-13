f8fe02d2a2e959b133049f5f01f220232afc9587
xchbx <ceina.ellison@gmail.com>
2025-02-13 18:52:55 -0500

    header component built

---
+++ src/Title.js
@@ -0,0 +1,14 @@
+import './Title.css';
+
+function Title() {
+	return (
+		<div>
+			<h1 className="Title">SFPOPOS</h1>
+			<div className="Title-Subtitle">
+				San Francisco Privately Owned Public Open Spaces
+			</div>
+		</div>
+	)
+};
+
+export default Title;
