b0f024c018afd9a4d6d22412346b5baf6aa66699
xchbx <ceina.ellison@gmail.com>
2025-02-18 17:58:47 -0500

    dynamic content & footer

--- src/POPOSList.js
+++ src/POPOSList.js
@@ -1,38 +1,23 @@
 import POPOSSpace from "./POPOSSpace";
 import "./POPOSList.css";
+import data from "./sfpopos-data.json"
 
 function POPOSList() {
+
+    const spaces = data.map(({ title, address, images, hours }) => {
+        return (
+            <POPOSSpace
+                key={title} // The title could be a key, since each title is unique
+                name={title}
+                address={address}
+                image={images[0]}
+                hours={hours}
+            />
+        )
+    });
     return (
         <div className="POPOSList">
-            <POPOSSpace 
-            name="50 California Street"
-            address="50 California St."
-            image="50-california-st.jpg"
-            />
-            <POPOSSpace 
-            name="100 Pine"
-            address="100 Pine"
-            image="100-pine.jpg"
-            />
-            <POPOSSpace 
-            name="101 California"
-            address="101 California"
-            image="101-california.jpg"
-            />
-            <POPOSSpace 
-            name="343 Sansome Roof Garden"
-            address="343 Sansome Roof Garden"
-            image="343-sansome-roof-garden.jpg"/>
-            <POPOSSpace 
-            name="555 California"
-            address="555 California"
-            image="555-california.jpg"
-            />
-            <POPOSSpace 
-            name="525 Market Street Plaza"
-            address="525 Market Street Plaza"
-            image="525-market-street-plaza.jpg"
-            />
+            { spaces }
         </div>
     )
 };
--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -2,16 +2,17 @@
 import "./POPOSSpace.css"
 
 function POPOSSpace(props) {
-    const { name, image, address } = props;
+    const { name, image, address, hours } = props;
     return (
         <div className="POPOSSpace">
+            <h1>{name}</h1>
             <img src={`${process.env.PUBLIC_URL}/images/${image}`} 
             width="300" 
             height="300" 
             alt="50 California St"
             />
-            <h1>{name}</h1>
             <div>{address}</div>
+            <div className="hours">{hours}</div>
         </div>
     )
 };
