c9fdfeb70c33fecfbb515353849e86ce5520b2ed
xchbx <ceina.ellison@gmail.com>
2025-02-20 18:10:24 -0500

    style details page

--- src/components/POPOSDetails/POPOSDetails.js
+++ src/components/POPOSDetails/POPOSDetails.js
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import data from "../../sfpopos-data.json";
+import "./POPOSDetails.css"
 
 function POPOSDetails(props) {
     const params = useParams();
@@ -7,17 +8,17 @@ function POPOSDetails(props) {
     const { images, title, desc, hours, features, geo } = data[id]
 
     return (
-        <div>
-            <div>
+        <div className="POPOSDetails">
+            <div className="POPOSDetails-image"> 
                 <img src={`${process.env.PUBLIC_URL}images/${images[0]}`} alt={title}/>
             </div>
 
-            <div>
-                <h1>{ title }</h1>
-                <p>{ desc }</p>
-                <p>{ hours }</p>
-                <p>{ features }</p>
-                <p>{ geo.lat } { geo.lon }</p>
+            <div className="POPOSDetails-info">
+                <h1 className="POPOSDetails-title">{ title }</h1>
+                <p className="POPOSDetails-desc">{ desc }</p>
+                <p className="POPOSDetails-hours">{ hours }</p>
+                <p className="POPOSDetails-features">{ features }</p>
+                <p className="POPOSDetails-geo">{ geo.lat } { geo.lon }</p>
             </div>
 
         </div>
